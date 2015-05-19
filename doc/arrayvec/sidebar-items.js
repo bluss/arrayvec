a5ae7afcf6dab319b3019abe9f508c92da7f4b79
root <root@localhost>
2015-05-19 17:23:14 +0200

    Update docs

---
+++ doc/arrayvec/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["ArrayVec","A vector with a fixed capacity."],["IntoIter","By-value iterator for ArrayVec."]],"trait":[["Array","Trait for fixed size arrays."]]});
---
+++ doc/implementors/core/iter/trait.IntoIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['arrayvec'] = ["impl&lt;'a, A: <a class='trait' href='arrayvec/trait.Array.html' title='arrayvec::Array'>Array</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.IntoIterator.html' title='core::iter::IntoIterator'>IntoIterator</a> for &amp;'a <a class='struct' href='arrayvec/struct.ArrayVec.html' title='arrayvec::ArrayVec'>ArrayVec</a>&lt;A&gt;","impl&lt;'a, A: <a class='trait' href='arrayvec/trait.Array.html' title='arrayvec::Array'>Array</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.IntoIterator.html' title='core::iter::IntoIterator'>IntoIterator</a> for &amp;'a mut <a class='struct' href='arrayvec/struct.ArrayVec.html' title='arrayvec::ArrayVec'>ArrayVec</a>&lt;A&gt;","impl&lt;A: <a class='trait' href='arrayvec/trait.Array.html' title='arrayvec::Array'>Array</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.IntoIterator.html' title='core::iter::IntoIterator'>IntoIterator</a> for <a class='struct' href='arrayvec/struct.ArrayVec.html' title='arrayvec::ArrayVec'>ArrayVec</a>&lt;A&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
