faa05e2ca861bc3061d064e7cfd2b497f1ad513d
root <root@localhost>
2015-05-19 16:48:22 +0200

    More sizes

--- src/lib.rs
+++ src/lib.rs
@@ -58,7 +58,7 @@ macro_rules! fix_array_impl_recursive {
 
 fix_array_impl_recursive!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
                           16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
-                          32, 48, 64, 96, 128,);
+                          32, 40, 48, 56, 64, 72, 96, 128, 160, 192, 224,);
 
 /// A vector with a fixed capacity.
 ///
