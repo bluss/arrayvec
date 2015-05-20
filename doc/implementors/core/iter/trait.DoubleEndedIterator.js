17a3e2eb1e8d9c2f2ffb70cd1638eb50af15241c
root <root@localhost>
2015-05-20 17:23:17 +0200

    Update docs

---
+++ doc/implementors/core/iter/trait.DoubleEndedIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['arrayvec'] = ["impl&lt;A: <a class='trait' href='arrayvec/trait.Array.html' title='arrayvec::Array'>Array</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.DoubleEndedIterator.html' title='core::iter::DoubleEndedIterator'>DoubleEndedIterator</a> for <a class='struct' href='arrayvec/struct.IntoIter.html' title='arrayvec::IntoIter'>IntoIter</a>&lt;A&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
