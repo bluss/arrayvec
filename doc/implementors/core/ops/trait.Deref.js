a58819d999acc68837f761de2ef13600616eeda6
root <root@localhost>
2015-05-21 11:59:42 +0200

    Update docs

--- doc/implementors/core/ops/trait.Deref.js
+++ doc/implementors/core/ops/trait.Deref.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['arrayvec'] = ["impl&lt;A: <a class='trait' href='arrayvec/trait.Array.html' title='arrayvec::Array'>Array</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='arrayvec/struct.ArrayVec.html' title='arrayvec::ArrayVec'>ArrayVec</a>&lt;A&gt;",];
+implementors['nodrop'] = ["impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='nodrop/struct.NoDrop.html' title='nodrop::NoDrop'>NoDrop</a>&lt;T&gt;",];implementors['arrayvec'] = ["impl&lt;A: <a class='trait' href='arrayvec/trait.Array.html' title='arrayvec::Array'>Array</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='arrayvec/struct.ArrayVec.html' title='arrayvec::ArrayVec'>ArrayVec</a>&lt;A&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
