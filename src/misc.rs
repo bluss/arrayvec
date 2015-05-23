1eec0d483477393f8dfbd2eab70ffadad0a5e624
root <root@localhost>
2015-05-23 12:43:45 +0200

    Make sure the methods on RangeArgument can be inlined

--- src/misc.rs
+++ src/misc.rs
@@ -8,9 +8,11 @@ use std::ops::{
 /// **RangeArgument** is implemented by Rust's built-in range types, produced
 /// by range syntax like `..`, `a..`, `..b` or `c..d`.
 pub trait RangeArgument {
+    #[inline]
     #[doc(hidden)]
     /// Start index (inclusive)
     fn start(&self) -> Option<usize> { None }
+    #[inline]
     #[doc(hidden)]
     /// End index (exclusive)
     fn end(&self) -> Option<usize> { None }
@@ -20,15 +22,19 @@ pub trait RangeArgument {
 impl RangeArgument for RangeFull {}
 
 impl RangeArgument for RangeFrom<usize> {
+    #[inline]
     fn start(&self) -> Option<usize> { Some(self.start) }
 }
 
 impl RangeArgument for RangeTo<usize> {
+    #[inline]
     fn end(&self) -> Option<usize> { Some(self.end) }
 }
 
 impl RangeArgument for Range<usize> {
+    #[inline]
     fn start(&self) -> Option<usize> { Some(self.start) }
+    #[inline]
     fn end(&self) -> Option<usize> { Some(self.end) }
 }
 
