3175d97f80f046a4a4366b90fee15aeb7c5f4a68
root <root@localhost>
2015-05-23 03:12:27 +0200

    Implement ArrayVec::clear

--- src/lib.rs
+++ src/lib.rs
@@ -107,6 +107,10 @@ impl<A: Array> ArrayVec<A> {
     #[inline]
     pub fn capacity(&self) -> usize { A::capacity() }
 
+    /// Remove all elements in the vector.
+    pub fn clear(&mut self) {
+        while let Some(_) = self.pop() { }
+    }
 
     /// Push **element** to the end of the vector.
     ///
