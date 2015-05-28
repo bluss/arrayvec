f72181d318448fe591a5641611022b8be790a9b3
root <root@localhost>
2015-05-28 15:01:47 +0200

    Update docs

--- doc/implementors/core/iter/trait.FromIterator.js
+++ doc/implementors/core/iter/trait.FromIterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['arrayvec'] = ["impl&lt;A: <a class='trait' href='arrayvec/trait.Array.html' title='arrayvec::Array'>Array</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.FromIterator.html' title='core::iter::FromIterator'>FromIterator</a>&lt;A&gt; for <a class='struct' href='arrayvec/struct.ArrayVec.html' title='arrayvec::ArrayVec'>ArrayVec</a>&lt;A&gt;",];
+implementors['arrayvec'] = ["impl&lt;A: <a class='trait' href='arrayvec/trait.Array.html' title='arrayvec::Array'>Array</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.FromIterator.html' title='core::iter::FromIterator'>FromIterator</a>&lt;A::Item&gt; for <a class='struct' href='arrayvec/struct.ArrayVec.html' title='arrayvec::ArrayVec'>ArrayVec</a>&lt;A&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
