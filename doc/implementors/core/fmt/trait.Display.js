16b9b10d31db0b8cf7d5f6238781f36024946388
root <root@localhost>
2015-06-12 15:22:37 +0200

    Update docs

---
+++ doc/implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['void'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='enum' href='void/enum.Void.html' title='void::Void'>Void</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
