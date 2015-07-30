b3a085ee41dc1d052acdc344a0887b03e0c4f0f1
root <root@localhost>
2015-07-30 16:22:17 +0200

    Update docs

--- doc/implementors/core/hash/trait.Hash.js
+++ doc/implementors/core/hash/trait.Hash.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['arrayvec'] = ["impl&lt;A: <a class='trait' href='arrayvec/trait.Array.html' title='arrayvec::Array'>Array</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='arrayvec/struct.ArrayVec.html' title='arrayvec::ArrayVec'>ArrayVec</a>&lt;A&gt;",];
+implementors['arrayvec'] = ["impl&lt;A: <a class='trait' href='arrayvec/trait.Array.html' title='arrayvec::Array'>Array</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='arrayvec/struct.ArrayVec.html' title='arrayvec::ArrayVec'>ArrayVec</a>&lt;A&gt; <span class='where'>where A::Item: <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a></span>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
