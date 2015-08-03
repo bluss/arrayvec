cc04493d0eaeda46fbf25604e942558e876c9682
root <root@localhost>
2015-08-03 14:27:57 +0200

    Update docs

---
+++ doc/implementors/std/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['void'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='void/enum.Void.html' title='void::Void'>Void</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
