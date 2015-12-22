440bcbf66b82d26e29bd3b801112055edb016aca
bluss <bluss@users.noreply.github.com>
2015-12-22 08:07:18 +0100

    Add ArrayVec::dispose()

--- src/lib.rs
+++ src/lib.rs
@@ -57,9 +57,7 @@ pub struct ArrayVec<A: Array> {
 
 impl<A: Array> Drop for ArrayVec<A> {
     fn drop(&mut self) {
-        // clear all elements
-        while let Some(_) = self.pop() {
-        }
+        self.clear();
 
         // NoDrop inhibits array's drop
         // panic safety: NoDrop::drop will trigger on panic, so the inner
@@ -339,6 +337,12 @@ impl<A: Array> ArrayVec<A> {
                 Ok(array)
             }
         }
+    }
+
+    /// Dispose of `self` without the overwriting that is needed in Drop.
+    pub fn dispose(mut self) {
+        self.clear();
+        mem::forget(self);
     }
 }
 
