cd1fafda6b985a9a2ab7b447ebe1a619997b5582
bluss <bluss@users.noreply.github.com>
2016-02-06 01:14:17 +0100

    Update docs

--- doc/implementors/core/ops/trait.Fn.js
+++ doc/implementors/core/ops/trait.Fn.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['odds'] = ["impl&lt;'a, T, R&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Fn.html' title='core::ops::Fn'>Fn</a>&lt;<a class='primitive' href='https://doc.rust-lang.org/nightly/std/primitive.tuple.html'>(T,)</a>&gt; for <a class='struct' href='odds/struct.Fix.html' title='odds::Fix'>Fix</a>&lt;'a, T, R&gt;",];
+implementors['odds'] = ["impl&lt;'a, T, R&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Fn.html' title='core::ops::Fn'>Fn</a>&lt;<a class='primitive' href='https://doc.rust-lang.org/nightly/std/primitive.tuple.html'>(</a>T,<a class='primitive' href='https://doc.rust-lang.org/nightly/std/primitive.tuple.html'>)</a>&gt; for <a class='struct' href='odds/struct.Fix.html' title='odds::Fix'>Fix</a>&lt;'a, T, R&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- doc/implementors/core/ops/trait.FnMut.js
+++ doc/implementors/core/ops/trait.FnMut.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['odds'] = ["impl&lt;'a, T, R&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.FnMut.html' title='core::ops::FnMut'>FnMut</a>&lt;<a class='primitive' href='https://doc.rust-lang.org/nightly/std/primitive.tuple.html'>(T,)</a>&gt; for <a class='struct' href='odds/struct.Fix.html' title='odds::Fix'>Fix</a>&lt;'a, T, R&gt;",];
+implementors['odds'] = ["impl&lt;'a, T, R&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.FnMut.html' title='core::ops::FnMut'>FnMut</a>&lt;<a class='primitive' href='https://doc.rust-lang.org/nightly/std/primitive.tuple.html'>(</a>T,<a class='primitive' href='https://doc.rust-lang.org/nightly/std/primitive.tuple.html'>)</a>&gt; for <a class='struct' href='odds/struct.Fix.html' title='odds::Fix'>Fix</a>&lt;'a, T, R&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
