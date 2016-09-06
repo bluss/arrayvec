17ef395dc6a5e403f359cd9582d9226affc3be26
bluss <bluss@users.noreply.github.com>
2016-09-06 23:41:42 +0200

    Update

--- doc/implementors/odds/string/trait.StrSlice.js
+++ doc/implementors/odds/string/trait.StrSlice.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["odds"] = [];implementors["arrayvec"] = [];implementors["arrayvec"] = [];
+implementors["odds"] = [];implementors["arrayvec"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
