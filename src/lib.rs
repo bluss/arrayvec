6ea8e8c0bf13f8662a6a0a81ed829b3db49bfa65
bluss <bluss@users.noreply.github.com>
2017-08-06 18:23:35 +0200

    DOC: Update root crate url

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@
 //!
 //! This version of arrayvec requires Rust 1.15 or later.
 //!
-#![doc(html_root_url="https://docs.rs/arrayvec/0.3/")]
+#![doc(html_root_url="https://docs.rs/arrayvec/0.4/")]
 #![cfg_attr(not(feature="std"), no_std)]
 extern crate odds;
 extern crate nodrop;
