74745a9756df22e7f87695d0617790f21556b901
bluss <bluss@users.noreply.github.com>
2018-12-16 18:32:34 +0100

    DOC: Update minimum rust requirement to Rust 1.24
    Rust 1.24 is on debian stable. It is compatible with crossbeam (Rust 1.26),
    the rdep with most downloads.

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@
 //!
 //! ## Rust Version
 //!
-//! This version of arrayvec requires Rust 1.13 or later.
+//! This version of arrayvec requires Rust 1.24 or later.
 //!
 #![doc(html_root_url="https://docs.rs/arrayvec/0.4/")]
 #![cfg_attr(not(feature="std"), no_std)]
