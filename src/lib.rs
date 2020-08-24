55c14c90865cabd233158ceca16295233de052e9
Giacomo Stevanato <giaco.stevanato@gmail.com>
2020-08-24 22:44:41 +0200

    Added rayon feature and rayon::iter::IntoParallelIterator impl for ArrayVec

--- src/lib.rs
+++ src/lib.rs
@@ -49,6 +49,11 @@ use crate::maybe_uninit::MaybeUninit;
 #[cfg(feature="serde")]
 use serde::{Serialize, Deserialize, Serializer, Deserializer};
 
+#[cfg(feature="rayon")]
+use rayon::iter::plumbing::{bridge, Consumer, Producer, ProducerCallback, UnindexedConsumer};
+#[cfg(feature="rayon")]
+use rayon::iter::{IntoParallelIterator, ParallelIterator, IndexedParallelIterator};
+
 mod array;
 mod array_string;
 mod char;
@@ -1194,5 +1199,174 @@ impl<'de, T: Deserialize<'de>, A: Array<Item=T>> Deserialize<'de> for ArrayVec<A
         }
 
         deserializer.deserialize_seq(ArrayVecVisitor::<T, A>(PhantomData))
+    }
+}
+
+// Adapted from `rayon/vec.rs`
+
+#[cfg(feature="rayon")]
+/// Parallel iterator that moves out of an `ArrayVec`.
+#[derive(Debug, Clone)]
+pub struct IntoParIter<T: Send, A: Array<Item = T> + Send> {
+    vec: ArrayVec<A>,
+}
+
+#[cfg(feature="rayon")]
+impl<T, A> IntoParallelIterator for ArrayVec<A> 
+where 
+    T: Send,
+    A: Array<Item = T> + Send,
+    <A as Array>::Index: Send
+{
+    type Item = T;
+    type Iter = IntoParIter<T, A>;
+
+    fn into_par_iter(self) -> Self::Iter {
+        IntoParIter { vec: self }
+    }
+}
+
+#[cfg(feature="rayon")]
+impl<T, A> ParallelIterator for IntoParIter<T, A>
+where 
+    T: Send,
+    A: Array<Item = T> + Send,
+    <A as Array>::Index: Send
+{
+    type Item = T;
+
+    fn drive_unindexed<C>(self, consumer: C) -> C::Result
+    where
+        C: UnindexedConsumer<Self::Item>,
+    {
+        bridge(self, consumer)
+    }
+
+    fn opt_len(&self) -> Option<usize> {
+        Some(self.len())
+    }
+}
+
+#[cfg(feature="rayon")]
+impl<T, A> IndexedParallelIterator for IntoParIter<T, A>
+where 
+    T: Send,
+    A: Array<Item = T> + Send,
+    <A as Array>::Index: Send
+{
+    fn drive<C>(self, consumer: C) -> C::Result
+    where
+        C: Consumer<Self::Item>,
+    {
+        bridge(self, consumer)
+    }
+
+    fn len(&self) -> usize {
+        self.vec.len()
+    }
+
+    fn with_producer<CB>(mut self, callback: CB) -> CB::Output
+    where
+        CB: ProducerCallback<Self::Item>,
+    {
+        // The producer will move or drop each item from its slice, effectively taking ownership of
+        // them.  When we're done, the vector only needs to free its buffer.
+        unsafe {
+            // Make the `ArrayVec` forget about the actual items.
+            let len = self.vec.len();
+            self.vec.set_len(0);
+
+            // Get a correct borrow, then extend it to the original length.
+            let mut slice = self.vec.as_mut_slice();
+            slice = std::slice::from_raw_parts_mut(slice.as_mut_ptr(), len);
+
+            callback.callback(ArrayVecProducer { slice })
+        }
+    }
+}
+
+/// ////////////////////////////////////////////////////////////////////////
+
+#[cfg(feature="rayon")]
+struct ArrayVecProducer<'data, T: Send> {
+    slice: &'data mut [T],
+}
+
+#[cfg(feature="rayon")]
+impl<'data, T: 'data + Send> Producer for ArrayVecProducer<'data, T> {
+    type Item = T;
+    type IntoIter = SliceDrain<'data, T>;
+
+    fn into_iter(mut self) -> Self::IntoIter {
+        // replace the slice so we don't drop it twice
+        let slice = std::mem::replace(&mut self.slice, &mut []);
+        SliceDrain {
+            iter: slice.iter_mut(),
+        }
+    }
+
+    fn split_at(mut self, index: usize) -> (Self, Self) {
+        // replace the slice so we don't drop it twice
+        let slice = std::mem::replace(&mut self.slice, &mut []);
+        let (left, right) = slice.split_at_mut(index);
+        (ArrayVecProducer { slice: left }, ArrayVecProducer { slice: right })
+    }
+}
+
+#[cfg(feature="rayon")]
+impl<'data, T: 'data + Send> Drop for ArrayVecProducer<'data, T> {
+    fn drop(&mut self) {
+        SliceDrain {
+            iter: self.slice.iter_mut(),
+        };
+    }
+}
+
+/// ////////////////////////////////////////////////////////////////////////
+
+// like std::vec::Drain, without updating a source Vec
+#[cfg(feature="rayon")]
+struct SliceDrain<'data, T> {
+    iter: std::slice::IterMut<'data, T>,
+}
+
+#[cfg(feature="rayon")]
+impl<'data, T: 'data> Iterator for SliceDrain<'data, T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<T> {
+        let ptr = self.iter.next()?;
+        Some(unsafe { std::ptr::read(ptr) })
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let len = self.len();
+        (len, Some(len))
+    }
+}
+
+#[cfg(feature="rayon")]
+impl<'data, T: 'data> DoubleEndedIterator for SliceDrain<'data, T> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        let ptr = self.iter.next_back()?;
+        Some(unsafe { std::ptr::read(ptr) })
+    }
+}
+
+#[cfg(feature="rayon")]
+impl<'data, T: 'data> ExactSizeIterator for SliceDrain<'data, T> {
+    fn len(&self) -> usize {
+        self.iter.len()
+    }
+}
+
+#[cfg(feature="rayon")]
+impl<'data, T: 'data> Drop for SliceDrain<'data, T> {
+    fn drop(&mut self) {
+        for ptr in &mut self.iter {
+            unsafe {
+                std::ptr::drop_in_place(ptr);
+            }
+        }
     }
 }
