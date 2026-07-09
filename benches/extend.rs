a13bb9cd6bf60e23f7ad6621ccc53dcafa0ff6ae
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2026-07-09 02:10:35 +0200

    bench: Revitalise extend benchmarks
    Use std::hint::black_box (From Rust 1.6x), and use it more; this should
    make the benchmarks at least a little bit more representative.

--- benches/extend.rs
+++ benches/extend.rs
@@ -1,68 +1,72 @@
-
 extern crate arrayvec;
 #[macro_use] extern crate bencher;
 
 use std::io::Write;
+use std::hint::black_box;
+
+const CAP: usize = 1024 * 5;
 
 use arrayvec::ArrayVec;
-
 use bencher::Bencher;
-use bencher::black_box;
 
 fn extend_with_constant(b: &mut Bencher) {
-    let mut v = ArrayVec::<u8, 512>::new();
-    let cap = v.capacity();
+    let mut v = ArrayVec::<u8, CAP>::new();
     b.iter(|| {
         v.clear();
-        let constant = black_box(1);
-        v.extend((0..cap).map(move |_| constant));
-        v[511]
+        let constant = black_box(255u8);
+        v.extend((0..CAP).map(move |_| constant));
+        black_box(v.as_ptr());
+        black_box(v[CAP - 1])
     });
     b.bytes = v.capacity() as u64;
 }
 
 fn extend_with_range(b: &mut Bencher) {
-    let mut v = ArrayVec::<u8, 512>::new();
-    let cap = v.capacity();
+    let mut v = ArrayVec::<u8, CAP>::new();
     b.iter(|| {
         v.clear();
-        let range = 0..cap;
-        v.extend(range.map(|x| black_box(x as _)));
-        v[511]
+        let r = black_box((0..CAP).map(|x| x as u8));
+        v.extend(r);
+        black_box(v.as_ptr());
+        black_box(v[CAP - 1])
     });
     b.bytes = v.capacity() as u64;
 }
 
 fn extend_with_slice(b: &mut Bencher) {
-    let mut v = ArrayVec::<u8, 512>::new();
-    let data = [1; 512];
+    let data = [1u8; CAP];
+    let mut v = ArrayVec::<u8, CAP>::new();
     b.iter(|| {
         v.clear();
-        let iter = data.iter().map(|&x| x);
-        v.extend(iter);
-        v[511]
+        let data = black_box(&data[..]);
+        v.extend(data.iter().copied());
+        black_box(v.as_ptr());
+        black_box(v[CAP - 1])
     });
     b.bytes = v.capacity() as u64;
 }
 
 fn extend_with_write(b: &mut Bencher) {
-    let mut v = ArrayVec::<u8, 512>::new();
-    let data = [1; 512];
+    let data = [1u8; CAP];
+    let mut v = ArrayVec::<u8, CAP>::new();
     b.iter(|| {
         v.clear();
-        v.write(&data[..]).ok();
-        v[511]
+        let data = black_box(&data[..]);
+        v.write(data).ok();
+        black_box(v[CAP - 1])
     });
     b.bytes = v.capacity() as u64;
 }
 
 fn extend_from_slice(b: &mut Bencher) {
-    let mut v = ArrayVec::<u8, 512>::new();
-    let data = [1; 512];
+    let data = [1u8; CAP];
+    let mut v = ArrayVec::<u8, CAP>::new();
     b.iter(|| {
         v.clear();
-        v.try_extend_from_slice(&data).ok();
-        v[511]
+        let data = black_box(&data[..]);
+        v.try_extend_from_slice(data).ok();
+        black_box(v.as_ptr());
+        black_box(v[CAP - 1]);
     });
     b.bytes = v.capacity() as u64;
 }
